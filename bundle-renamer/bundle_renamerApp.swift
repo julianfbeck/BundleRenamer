deac307647df21201961ea0f64a6cb113eb3c608
Julian Beck <ju-fa-beck@t-online.de>
2023-05-09 14:51:40 +0200

    Initial Commit

---
+++ bundle-renamer/bundle_renamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bundle_renamerApp.swift
+//  bundle-renamer
+//
+//  Created by Julian Beck on 09.05.23.
+//
+
+import SwiftUI
+
+@main
+struct bundle_renamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
